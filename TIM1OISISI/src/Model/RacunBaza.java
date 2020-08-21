876659098e440563f0fc9ffbf820824f93c84a1c
jana <janajovanovic@uns.ac.rs>
2020-08-21 22:35:28 +0200

    izmena racun baza

--- TIM1OISISI/src/Model/RacunBaza.java
+++ TIM1OISISI/src/Model/RacunBaza.java
@@ -1,6 +1,5 @@
 package Model;
 
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -16,28 +15,24 @@ import javax.swing.JOptionPane;
 public class RacunBaza {
 	
 	
-	// promenjive za rad sa bazom
+	
 		private static Connection connect = null;
 		private static PreparedStatement preparedStatement = null;
 		private static ResultSet resultSet = null;
 
-		// podaci za bazu
+		
 		private static String url = "jdbc:mysql://localhost:3306/apoteka";
 		private static String username = "root";
 		private static String password = "root";
 
-		// kolekcije sa kojima radimo u program
-		// bolja i brža varijanta u odnosu na listu, kljuè da bude ID Racuna!
+		
 		public static HashMap<Integer, RacunKlasa> racunHash = new HashMap<Integer, RacunKlasa>();
 
-		/************************** GLAVNE METODE **************************/
+		
 
 		public static void start() throws Exception {
-			Class.forName("com.mysql.jdbc.Driver"); // loadovanje drivera za bazu
-			connect = DriverManager.getConnection(url, username, password); // uspostavljanje
-																			// konekcije
-																			// na
-																			// bazu
+			Class.forName("com.mysql.jdbc.Driver"); 
+			connect = DriverManager.getConnection(url, username, password); 
 		}
 
 		private static void close() {
@@ -57,321 +52,324 @@ public class RacunBaza {
 			}
 		}
 
-/********************* RAD SA Racunima **************************/
+	
 
-public boolean importRacun() {
-	boolean res = false;
+		public boolean importRacun() {
+			boolean res = false;
 
-	try {
-		start(); // da se nakaèi na bazu
+			try {
+				start(); // da se nakaèi na bazu
 
-		preparedStatement = connect
-				.prepareStatement("select * from racun"); // priprema sql
-															// upita
-		resultSet = preparedStatement.executeQuery(); // izvršavanje
-														// pripremljenog sql
-														// upita
-
-		// kupimo rezultate sql upita i dodajemo ih u kolekciju Korisnik
-		int i = 0;
-		while (resultSet.next()) {
-			i++;
-
-			RacunKlasa p = new RacunKlasa();
-		
-			Calendar datum = Calendar.getInstance();
-			
-			p.setDatum(datum);
-			
-			String lekS = resultSet.getString("listaproizvoda");
-			
-			String[] lista = lekS.split("-");
-			HashMap<String, LekRecept> lekovi = new HashMap<String, LekRecept> ();
-			
-			for (String spisak : lista) {
-			
-				String[] lek = spisak.split("\\?");
-				LekRecept l = new LekRecept();
-				l.setIme(lek[0]);
-				l.setKolicina(Integer.parseInt(lek[1]));
+				preparedStatement = connect
+						.prepareStatement("select * from racun"); 
+				resultSet = preparedStatement.executeQuery(); 
+																
 				
-				lekovi.put(l.getIme(), l);
+				int i = 0;
+				while (resultSet.next()) {
+					i++;
+
+					RacunKlasa p = new RacunKlasa();
+				
+					p.setSifra(resultSet.getInt("sifra"));
+					
+			
+					p.setApotekar(resultSet.getString("apotekar"));
+					p.setKupac(resultSet.getString("kupac"));
+				
+					Calendar datum = Calendar.getInstance();
+					
+					p.setDatum(datum);
+					
+					String lekS = resultSet.getString("listaproizvoda");
+					
+					String[] lista = lekS.split("-");
+					HashMap<String, LekRecept> lekovi = new HashMap<String, LekRecept> ();
+					
+					for (String spisak : lista) {
+					
+						String[] lek = spisak.split("\\?");
+						LekRecept l = new LekRecept();
+						l.setIme(lek[0]);
+						l.setKolicina(Integer.parseInt(lek[1]));
+						
+						lekovi.put(l.getIme(), l);
+					}
+					p.setLekovi(lekovi);
+					
+					p.setUkupno(resultSet.getFloat("ukupno"));
+					
+					p.setObrisan(resultSet.getBoolean("obrisan"));
+					
+					racunHash.put(p.getSifra(), p);
+				}
+
+				res = true; // ako nigde do sada nije pukao znaèi da se uspešno
+							// izvršilo
+				
+			} catch (Exception e) {
+				JOptionPane.showMessageDialog(new JFrame(),
+						"Desila se greška pri importu Racuna!");
+			} finally {
+				close(); // da se otkaèi sa baze
 			}
-			p.setLekovi(lekovi);
+
+			return res;
+		}
+
+		public boolean dodajRacun(RacunKlasa p) {
+
+			try {
+				start();
+
+				preparedStatement = connect
+						.prepareStatement("insert into racun(sifra,apotekar,kupac,datum,listaproizvoda,ukupno,obrisan) values (? ,? ,?, ?, ?, ?, ?)");
+
+				preparedStatement.setInt(1, p.getSifra());
+
+				preparedStatement.setString(2, p.getApotekar());
+				preparedStatement.setString(3, p.getKupac());
 			
-			p.setUkupno(resultSet.getFloat("ukupno"));
+
+				java.sql.Date d = new java.sql.Date(p.getDatum().getTimeInMillis());
+				preparedStatement.setDate(4, d);
 			
-			p.setObrisan(resultSet.getBoolean("obrisan"));
-			
+				String listaLekova = "";
+				for (String i : p.getLekovi().keySet()) {
+					listaLekova +=  p.getLekovi().get(i).getIme()+"?"+p.getLekovi().get(i).getKolicina()+"?-";
+					
+				}
+				preparedStatement.setString(5, listaLekova);
+				preparedStatement.setFloat(6, p.getUkupno());
+				preparedStatement.setBoolean(7, p.getObrisan());
+				preparedStatement.execute();
+
+			} catch (Exception e) {
+				JOptionPane.showMessageDialog(new JFrame(),
+						"Desila se greška pri dodavanju Racuna!");
+				return false;
+			} finally {
+				close();
+			}
+
 			racunHash.put(p.getSifra(), p);
+			return true;
 		}
 
-		res = true; // ako nigde do sada nije pukao znaèi da se uspešno
-					// izvršilo
-		
-	} catch (Exception e) {
-		JOptionPane.showMessageDialog(new JFrame(),
-				"Desila se greška pri importu Racuna!");
-	} finally {
-		close(); // da se otkaèi sa baze
-	}
+		public boolean izmeniRacun(RacunKlasa p) {
+			try {
+				start();
 
-	return res;
-}
+				preparedStatement = connect
+						.prepareStatement("update racun set apotekar = ?,kupac = ?,  datum  = ?,listaproizvoda = ?, ukupno = ?,obrisan = ?  where sifra = ?");
 
-public boolean dodajRacun(RacunKlasa p) {
-
-	try {
-		start();
-
-		preparedStatement = connect
-				.prepareStatement("insert into racun(sifra,apotekar,kupac,datum,listaproizvoda,ukupno,obrisan) values (? ,? ,?, ?, ?, ?, ?)");
-
-		preparedStatement.setInt(1, p.getSifra());
-
-		preparedStatement.setString(2, p.getApotekar());
-		preparedStatement.setString(3, p.getKupac());
-	
-
-		java.sql.Date d = new java.sql.Date(p.getDatum().getTimeInMillis());
-		preparedStatement.setDate(4, d);
-	
-		String listaLekova = "";
-		for (String i : p.getLekovi().keySet()) {
-			listaLekova +=  p.getLekovi().get(i).getIme()+"?"+p.getLekovi().get(i).getKolicina()+"?-";
+				preparedStatement.setString(1, p.getApotekar());
+				preparedStatement.setString(2, p.getKupac());
 			
-		}
-		preparedStatement.setString(5, listaLekova);
-		preparedStatement.setFloat(6, p.getUkupno());
-		preparedStatement.setBoolean(7, p.getObrisan());
-		preparedStatement.execute();
 
-	} catch (Exception e) {
-		JOptionPane.showMessageDialog(new JFrame(),
-				"Desila se greška pri dodavanju Racuna!");
-		return false;
-	} finally {
-		close();
-	}
+				java.sql.Date d = new java.sql.Date(p.getDatum().getTimeInMillis());
+				preparedStatement.setDate(3, d);
+			    String listaLekova = "";
+				for (String i : p.getLekovi().keySet()) {
+					listaLekova +=  p.getLekovi().get(i).getIme()+"?"+p.getLekovi().get(i).getKolicina()+"?-";
+					
+				}
+				preparedStatement.setString(4, listaLekova);
+				preparedStatement.setFloat(5, p.getUkupno());
+				preparedStatement.setBoolean(6, p.getObrisan());
+				preparedStatement.executeUpdate();
 
-	racunHash.put(p.getSifra(), p);
-	return true;
-}
-
-public boolean izmeniRacun(RacunKlasa p) {
-	try {
-		start();
-
-		preparedStatement = connect
-				.prepareStatement("update racun set apotekar = ?,kupac = ?,  datum  = ?,listaproizvoda = ?, ukupno = ?,obrisan = ?  where sifra = ?");
-
-		preparedStatement.setString(1, p.getApotekar());
-		preparedStatement.setString(2, p.getKupac());
-	
-
-		java.sql.Date d = new java.sql.Date(p.getDatum().getTimeInMillis());
-		preparedStatement.setDate(3, d);
-	    String listaLekova = "";
-		for (String i : p.getLekovi().keySet()) {
-			listaLekova +=  p.getLekovi().get(i).getIme()+"?"+p.getLekovi().get(i).getKolicina()+"?-";
-			
-		}
-		preparedStatement.setString(4, listaLekova);
-		preparedStatement.setFloat(5, p.getUkupno());
-		preparedStatement.setBoolean(6, p.getObrisan());
-		preparedStatement.executeUpdate();
-
-	} catch (Exception e) {
-		JOptionPane.showMessageDialog(new JFrame(),
-				"Desila se greška pri izmeni Racuna!");
-		return false;
-	} finally {
-		close();
-	}
-
-	racunHash.remove(p.getSifra());
-	racunHash.put(p.getSifra(), p);
-
-	return true;
-}
-
-public void refresh() {
-	try {
-		start();
-
-		preparedStatement = connect
-				.prepareStatement("select * from racun"); // priprema
-															// sql upita
-		resultSet = preparedStatement.executeQuery(); // izvršavanje
-														// pripremljenog
-														// sql upita
-		racunHash.clear();
-		// kupimo rezultate sql upita i dodajemo ih u kolekciju
-		while (resultSet.next()) {
-	
-
-			RacunKlasa p = new RacunKlasa();
-			p.setSifra(resultSet.getInt("sifra"));
-			p.setApotekar(resultSet.getString("apotekar"));
-			p.setKupac(resultSet.getString("kupac"));
-		
-			Calendar datum = Calendar.getInstance();
-			datum.setTime(resultSet.getDate("datum"));
-			p.setDatum(datum);
-
-			String lekS = resultSet.getString("listaproizvoda");
-			String[] lista = lekS.split("-");
-			HashMap<String, LekRecept> lekovi = new HashMap<String, LekRecept> ();
-			
-			for (String spisak : lista) {
-			
-				String[] lek = spisak.split("\\?");
-				LekRecept l = new LekRecept();
-				l.setIme(lek[0]);
-				l.setKolicina(Integer.parseInt(lek[1]));
-				
-				lekovi.put(l.getIme(), l);
+			} catch (Exception e) {
+				JOptionPane.showMessageDialog(new JFrame(),
+						"Desila se greška pri izmeni Racuna!");
+				return false;
+			} finally {
+				close();
 			}
-			p.setLekovi(lekovi);
-			p.setUkupno(resultSet.getFloat("ukupno"));
-			p.setObrisan(resultSet.getBoolean("obrisan"));
+
+			racunHash.remove(p.getSifra());
 			racunHash.put(p.getSifra(), p);
+
+			return true;
 		}
 
-	} catch (Exception e) {
-		JOptionPane.showMessageDialog(new JFrame(),
-				"Desila se greška pri osvezavanju prikaza!");
-	} finally {
-		close();
-	}
-}
+		public void refresh() {
+			try {
+				start();
 
-public boolean ukloniRacun(Integer id) {
-	boolean res = false;
-
-	try {
-		start();
-		preparedStatement = connect
-				.prepareStatement("DELETE FROM racun WHERE sifra=?");
-		preparedStatement.setInt(1, id);
-		preparedStatement.execute();
-
-		racunHash.remove(id);
-		res = true;
-	} catch (Exception e) {
-		JOptionPane.showMessageDialog(new JFrame(),
-				"Desila se greška pri uklanjanju Racunaa!");
-		e.printStackTrace();
-	} finally {
-		close();
-	}
-
-	return res;
-}
-
-public List<HashMap<String, Object>> SelectQuery(String query) {
-	List<HashMap<String, Object>> res = new ArrayList<HashMap<String, Object>>();
-	int j = 0;
-	try {
-		start();
-	
-		preparedStatement = connect.prepareStatement(query);
-		
-
-		ResultSet rs = preparedStatement.executeQuery();
-		
-		while (rs.next()) {
-			j++;
-			RacunKlasa p = new RacunKlasa();
-			HashMap<String, Object> row = new HashMap<String, Object>();
-			row.put("sifra", rs.getInt("sifra"));
-			row.put("apotekar", rs.getString("apotekar"));
-			row.put("kupac", rs.getString("kupac"));
+				preparedStatement = connect
+						.prepareStatement("select * from racun"); // priprema
+																	// sql upita
+				resultSet = preparedStatement.executeQuery(); // izvršavanje
+																// pripremljenog
+																// sql upita
+				racunHash.clear();
+				// kupimo rezultate sql upita i dodajemo ih u kolekciju
+				while (resultSet.next()) {
 			
-			row.put("datum", rs.getDate("datum"));
-			row.put("lekovi", rs.getString("listaproizvoda"));
-			row.put("ukupno", rs.getFloat("ukupno"));
-			row.put("obrisan", rs.getBoolean("obrisan"));
-			res.add(row);
 
-		}
-
-	} catch (Exception e) {
-		JOptionPane.showMessageDialog(new JFrame(),
-				"Desila se greška prilikom upita u bazu Racun!");
-		e.printStackTrace();
-	} finally {
-		close();
-	}
-
-	if (j == 0)
-		return null;
-	else
-
-		return res;
-}
-
-public List<List<Object>> SelectQueryList(String query) {
-	List<List<Object>> res = new ArrayList<List<Object>>();
-	int j = 0;
-	try {
-		start();
-	
-		preparedStatement = connect.prepareStatement(query);
-	
-
-		ResultSet rs = preparedStatement.executeQuery();
-		
-		int columns = rs.getMetaData().getColumnCount();
-
-		while (rs.next()) {
-
-			j++;
-
-			List<Object> row = new ArrayList<Object>(columns);
-			for (int i = 1; i <= columns; i++) {
-				row.add(rs.getObject(i));
+					RacunKlasa p = new RacunKlasa();
+					p.setSifra(resultSet.getInt("sifra"));
+					p.setApotekar(resultSet.getString("apotekar"));
+					p.setKupac(resultSet.getString("kupac"));
 				
-			}
-			res.add(row);
-		
+					Calendar datum = Calendar.getInstance();
+					datum.setTime(resultSet.getDate("datum"));
+					p.setDatum(datum);
 
+					String lekS = resultSet.getString("listaproizvoda");
+					String[] lista = lekS.split("-");
+					HashMap<String, LekRecept> lekovi = new HashMap<String, LekRecept> ();
+					
+					for (String spisak : lista) {
+					
+						String[] lek = spisak.split("\\?");
+						LekRecept l = new LekRecept();
+						l.setIme(lek[0]);
+						l.setKolicina(Integer.parseInt(lek[1]));
+						
+						lekovi.put(l.getIme(), l);
+					}
+					p.setLekovi(lekovi);
+					p.setUkupno(resultSet.getFloat("ukupno"));
+					p.setObrisan(resultSet.getBoolean("obrisan"));
+					racunHash.put(p.getSifra(), p);
+				}
+
+			} catch (Exception e) {
+				JOptionPane.showMessageDialog(new JFrame(),
+						"Desila se greška pri osvezavanju prikaza!");
+			} finally {
+				close();
+			}
 		}
 
-	} catch (Exception e) {
-		JOptionPane.showMessageDialog(new JFrame(),
-				"Desila se greška prilikom upita u bazu Racun!");
-		e.printStackTrace();
-	} finally {
-		close();
+		public boolean ukloniRacun(Integer id) {
+			boolean res = false;
+
+			try {
+				start();
+				preparedStatement = connect
+						.prepareStatement("DELETE FROM racun WHERE sifra=?");
+				preparedStatement.setInt(1, id);
+				preparedStatement.execute();
+
+				racunHash.remove(id);
+				res = true;
+			} catch (Exception e) {
+				JOptionPane.showMessageDialog(new JFrame(),
+						"Desila se greška pri uklanjanju Racunaa!");
+				e.printStackTrace();
+			} finally {
+				close();
+			}
+
+			return res;
+		}
+
+		public List<HashMap<String, Object>> SelectQuery(String query) {
+			List<HashMap<String, Object>> res = new ArrayList<HashMap<String, Object>>();
+			int j = 0;
+			try {
+				start();
+			
+				preparedStatement = connect.prepareStatement(query);
+				
+
+				ResultSet rs = preparedStatement.executeQuery();
+				
+				while (rs.next()) {
+					j++;
+					RacunKlasa p = new RacunKlasa();
+					HashMap<String, Object> row = new HashMap<String, Object>();
+					row.put("sifra", rs.getInt("sifra"));
+					row.put("apotekar", rs.getString("apotekar"));
+					row.put("kupac", rs.getString("kupac"));
+					
+					row.put("datum", rs.getDate("datum"));
+					row.put("lekovi", rs.getString("listaproizvoda"));
+					row.put("ukupno", rs.getFloat("ukupno"));
+					row.put("obrisan", rs.getBoolean("obrisan"));
+					res.add(row);
+
+				}
+
+			} catch (Exception e) {
+				JOptionPane.showMessageDialog(new JFrame(),
+						"Desila se greška prilikom upita u bazu Racun!");
+				e.printStackTrace();
+			} finally {
+				close();
+			}
+
+			if (j == 0)
+				return null;
+			else
+
+				return res;
+		}
+
+		public List<List<Object>> SelectQueryList(String query) {
+			List<List<Object>> res = new ArrayList<List<Object>>();
+			int j = 0;
+			try {
+				start();
+			
+				preparedStatement = connect.prepareStatement(query);
+			
+
+				ResultSet rs = preparedStatement.executeQuery();
+				
+				int columns = rs.getMetaData().getColumnCount();
+
+				while (rs.next()) {
+
+					j++;
+		
+					List<Object> row = new ArrayList<Object>(columns);
+					for (int i = 1; i <= columns; i++) {
+						row.add(rs.getObject(i));
+						
+					}
+					res.add(row);
+				
+
+				}
+
+			} catch (Exception e) {
+				JOptionPane.showMessageDialog(new JFrame(),
+						"Desila se greška prilikom upita u bazu Racun!");
+				e.printStackTrace();
+			} finally {
+				close();
+			}
+
+			if (j == 0)
+				return null;
+			else
+
+				return res;
+		}
+
+		public boolean PostojiRacun(Integer id) {
+			return racunHash.containsKey(id);
+		}
+
+		public RacunKlasa GetRacun(Integer id) {
+			if (racunHash.containsKey(id))
+				return racunHash.get(id);
+
+			return null;
+		}
+
+		public HashMap<Integer, RacunKlasa> GetRacuni() {
+			return racunHash;
+		}
+
+		
+
+	public RacunBaza() {
+		// TODO Auto-generated constructor stub
 	}
-
-	if (j == 0)
-		return null;
-	else
-
-		return res;
-}
-
-public boolean PostojiRacun(Integer id) {
-	return racunHash.containsKey(id);
-}
-
-public RacunKlasa GetRacun(Integer id) {
-	if (racunHash.containsKey(id))
-		return racunHash.get(id);
-
-	return null;
-}
-
-public HashMap<Integer, RacunKlasa> GetRacuni() {
-	return racunHash;
-}
-
-
-
-public RacunBaza() {
-// TODO Auto-generated constructor stub
-}
 
 }
