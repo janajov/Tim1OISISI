ae54b3375d322b3a3d748f77596de75e678ce665
jana <janajovanovic@uns.ac.rs>
2020-08-21 12:10:36 +0200

    izvestaj lekova

--- TIM1OISISI/src/Interfejs/frmIzvestajLekova.java
+++ TIM1OISISI/src/Interfejs/frmIzvestajLekova.java
@@ -1,22 +1,44 @@
 package Interfejs;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JDialog;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JScrollPane;
 import javax.swing.JLabel;
-import java.awt.Color;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.TitledBorder;
 import javax.swing.table.DefaultTableModel;
 
-public class frmIzvestajLekova extends JDialog {
+import Model.LekBaza;
+import Model.LekKlasa;
+import Model.LekRecept;
+import Model.RacunBaza;
+import Model.ReceptBaza;
+import Model.ReceptKlasa;
 
-	private final JPanel contentPanelIzvestajLekova = new JPanel();
-	private JTable tableIzvestajLekova;
+
+public class frmIzvestajLekova extends JDialog {
+	private final JPanel contentPanel = new JPanel();
+	private JTable table;
+	
+	DefaultTableModel model;
+
+	LekBaza lekovi = new LekBaza();
+	ReceptBaza recepti = new ReceptBaza();
+	RacunBaza racuni = new RacunBaza();
+	JLabel lblSaldo;
 
 	/**
 	 * Launch the application.
@@ -35,47 +57,97 @@ public class frmIzvestajLekova extends JDialog {
 	 * Create the dialog.
 	 */
 	public frmIzvestajLekova() {
-		setTitle("Izvestaj svih prodatih lekova");
-		setBounds(100, 100, 821, 420);
-		getContentPane().setLayout(new BorderLayout());
-		contentPanelIzvestajLekova.setBackground(new Color(102, 205, 170));
-		contentPanelIzvestajLekova.setBorder(new EmptyBorder(5, 5, 5, 5));
-		getContentPane().add(contentPanelIzvestajLekova, BorderLayout.CENTER);
-		contentPanelIzvestajLekova.setLayout(null);
-		{
-			JScrollPane scrollPane = new JScrollPane();
-			scrollPane.setBounds(10, 11, 785, 280);
-			contentPanelIzvestajLekova.add(scrollPane);
+		getContentPane().setBackground(new Color(102, 205, 170));
+		setTitle("IZVE\u0160TAJ SVI PRODATIH LEKOVA");
+		setModal(true);
+		setBounds(100, 100, 841, 339);
+		getContentPane().setLayout(null);
+		lekovi.importLek();
+		recepti.importRecept();
+		racuni.importRacun();
+
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 11, 805, 214);
+		getContentPane().add(scrollPane);
+
+		table = new JTable();
+		scrollPane.setViewportView(table);
+		String[] header = new String[] { "Sifra", "Ime", "Proizvodjaè",
+				"Ide na recept", "Cena jednog leka", "Prodata kolicina",
+				"Ukupna zarada" };
+
+		model = new DefaultTableModel(new Object[][] {}, header);
+
+		table.setModel(model);
+
+		JButton btnPrikazi = new JButton("Prikazi");
+		btnPrikazi.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+
+				// lekovi.importLek();
 			
-			tableIzvestajLekova = new JTable();
-			tableIzvestajLekova.setModel(new DefaultTableModel(
-				new Object[][] {
-				},
-				new String[] {
-					"Sifra", "Ime", "Proizvodjac ", "Ide na recept", "Cena jednog leka", "Prodata kolicina", "Ukupna zarada"
+				try {
+					float ukupnaZaradaApoteke = 0;
+					int kolicina = 0;
+					float ukupno = 0;
+					model.setRowCount(0);
+					List<LekKlasa> lista1 = new ArrayList<LekKlasa>();
+					for (String i : lekovi.lekHash.keySet()) {
+						kolicina = 0;
+						for (Integer r : racuni.racunHash.keySet())
+
+							if (racuni.racunHash.get(r).getLekovi()
+									.containsKey(lekovi.lekHash.get(i).getIme())) {
+								LekRecept l = racuni.racunHash.get(r)
+										.getLekovi().get(lekovi.lekHash.get(i).getIme());
+
+								kolicina += l.getKolicina();
+							}
+						if (kolicina > 0) {
+							ukupno = kolicina * lekovi.lekHash.get(i).getCena();
+							ukupnaZaradaApoteke += ukupno;
+							lista1.add(lekovi.lekHash.get(i));
+							// for (ReceptKlasa obj : lista1) {
+							LekKlasa obj = lekovi.lekHash.get(i);
+							Object[] obj1 = new Object[] { obj.getSifra(),
+									obj.getIme(), obj.getProizvodjac(),
+									obj.getRecept(), obj.getCena(), kolicina,
+									ukupno, };
+							model.addRow(obj1);
+							// }
+
+						}
+					}
+
+					
+					lblSaldo.setText(Float.toString(ukupnaZaradaApoteke)+ "  dinara");
+					
+					table.setFillsViewportHeight(true);
+				} catch (Exception ex) {
+					System.out.println(ex.getMessage());
 				}
-			));
-			scrollPane.setViewportView(tableIzvestajLekova);
-		}
-		{
-			JButton btnPrikazi = new JButton("Prikazi");
-			btnPrikazi.setBounds(23, 348, 89, 23);
-			contentPanelIzvestajLekova.add(btnPrikazi);
-		}
-		{
-			JButton btnOdustani = new JButton("Odustani");
-			btnOdustani.setBounds(227, 348, 89, 23);
-			contentPanelIzvestajLekova.add(btnOdustani);
-		}
-		{
-			JLabel lblUkupnaZaradaApoteke = new JLabel("Ukupna zarada apoteke:");
-			lblUkupnaZaradaApoteke.setBounds(534, 302, 121, 14);
-			contentPanelIzvestajLekova.add(lblUkupnaZaradaApoteke);
-		}
-		{
-			JLabel lblSaldo = new JLabel("---------------");
-			lblSaldo.setBounds(711, 302, 46, 14);
-			contentPanelIzvestajLekova.add(lblSaldo);
-		}
+			}
+		});
+		btnPrikazi.setBounds(10, 266, 89, 23);
+		getContentPane().add(btnPrikazi);
+
+		JButton btnOdustani = new JButton("Odustani");
+		btnOdustani.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+			}
+		});
+		btnOdustani.setBounds(229, 266, 89, 23);
+		getContentPane().add(btnOdustani);
+		JLabel lblUkupnaZarada = new JLabel("Ukupna zarada apoteke:");
+		lblUkupnaZarada.setBounds(470, 236, 173, 14);
+		getContentPane().add(lblUkupnaZarada);
+
+		lblSaldo = new JLabel("--------------------------");
+		lblSaldo.setBounds(701, 236, 114, 14);
+		getContentPane().add(lblSaldo);
+		contentPanel.setLayout(null);
+
 	}
+
 }
